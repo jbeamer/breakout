f54e403175b142b1a49b09695e8e2e7f5ea5b038
Jonathan Beamer <jbeamer@gmail.com>
2020-03-13 11:53:58 -0700

    added a ball that checks for edges and bounces indefinitely

--- breakout.py
+++ breakout.py
@@ -9,6 +9,41 @@ PLAY_WIDTH = 600
 PLAY_HEIGHT = 400
 TOPLEFT_X = (SCREEN_WIDTH  - PLAY_WIDTH) // 2
 TOPLEFT_Y = (SCREEN_HEIGHT - PLAY_HEIGHT) // 2
+BACKGROUND_COLOR = (0, 0, 0)
+FONT_COLOR = (255, 255, 255)
+BORDER_COLOR = (255, 255, 255)
+BALL_COLOR = (255, 255, 255)
+BALL_RADIUS = 10
+
+class Point:
+    ''' a simple class to keep track of coordinates'''
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class BreakoutBall:
+    '''' BreakoutBall class '''
+    def __init__(self):
+        self.location = Point(TOPLEFT_X + PLAY_WIDTH / 2, TOPLEFT_Y + PLAY_HEIGHT / 2)
+        self.velocity = Point(1, 1)
+        
+    def move(self):
+        ''' moves the ball and bounces off of the constraints of our field '''
+        self.location.x += self.velocity.x
+        if self.location.x < TOPLEFT_X:
+            self.location.x = TOPLEFT_X
+            self.velocity.x = -self.velocity.x
+        if self.location.x > TOPLEFT_X + PLAY_WIDTH:
+            self.location.x = TOPLEFT_X + PLAY_WIDTH
+            self.velocity.x = -self.velocity.x
+
+        self.location.y += self.velocity.y
+        if self.location.y < TOPLEFT_Y:
+            self.location.y = TOPLEFT_Y
+            self.velocity.y = -self.velocity.y
+        if self.location.y > TOPLEFT_Y + PLAY_HEIGHT:
+            self.location.y = TOPLEFT_Y + PLAY_HEIGHT
+            self.velocity.y = -self.velocity.y
 
 class BreakoutGame:
     '''' BreakoutGame class '''
@@ -17,27 +52,29 @@ class BreakoutGame:
         pygame.font.init()
         pygame.display.set_caption("Breakout")
         self.surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.ball = BreakoutBall()
 
     def game_loop(self):
         ''' this function is called repeatedly to handle the functioning of the game experience '''
-        done = False
-        # handle any pending events.  All the piece moving and time advancing happens here
+        running = True
+        # handle any pending events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
+                running = False
+        self.ball.move()
         self.draw_window()
-        return done
+        return running
 
     def play(self):
         ''' the main function of the BreakoutGame object, sets up a clock to
             call our loop function repeatedly at 60 Hz '''
         # start game
-        done = False
+        running = True
         clock = pygame.time.Clock()
 
         # execute the main game loop at 60 Hz
-        while not done:
-            done = self.game_loop()
+        while running:
+            running = self.game_loop()
             clock.tick_busy_loop(60)
 
         # all done, do any clean up here:
@@ -46,18 +83,24 @@ class BreakoutGame:
     def draw_window(self):
         """ draw_window function """
         # start with a solid background
-        self.surface.fill((0, 0, 0))
+        self.surface.fill(BACKGROUND_COLOR)
 
         # write something at the top
         font = pygame.font.Font(None, 48)
-        label = font.render('Breakout', 1, (255, 255, 255))
+        label = font.render('Breakout', 1, FONT_COLOR)
         self.surface.blit(label, (TOPLEFT_X + PLAY_WIDTH/2 - label.get_width()/2, 30))
 
         # draw the border around the playing area:
         pygame.draw.rect(self.surface,
-                         (255, 255, 255),
+                         BORDER_COLOR,
                          (TOPLEFT_X - 2, TOPLEFT_Y - 2, PLAY_WIDTH + 4, PLAY_HEIGHT + 4),
                          2)
+
+        # draw the ball around the playing area:
+        pygame.draw.circle(self.surface,
+                           BALL_COLOR,
+                           (self.ball.location.x, self.ball.location.y),
+                           BALL_RADIUS)
 
         # update the screen with what we've drawn.
         pygame.display.flip()
